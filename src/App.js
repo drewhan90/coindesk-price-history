8ed5a31efd14c5d51a9e5c072328b51b38761088
Andrew Han <andrew@techsamurais.com>
2021-10-18 21:00:18 -0700

    added ISO, StartDate, EndDate input fields that will update state and when GetHistory button is clicked will trigger getPriceHistory

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import moment from 'moment'
 
-function App() {
+import Table from './components/Table'
+
+import './App.css';
+import fetchPriceHistory from './utils/fetchPriceHistory'
+
+const App = () =>  {
+  const [priceHistory, setPriceHistory] = React.useState()
+  const [iso, setIso] = React.useState('ETH')
+  const currentDate = moment().format('YYYY-DD-MM')
+  console.log({ currentDate })
+  // 2021-10-12T01:34
+  const [startDate, setStartDate] = React.useState(currentDate)
+  const [endDate, setEndDate] = React.useState(currentDate)
+
+  const getPriceHistory = async () => {
+    const _startDate = `${moment(startDate).format('YYYY-MM-DD')}T01:34`
+    const _endDate = `${moment(endDate).format('YYYY-MM-DD')}T01:34`
+    console.log(moment(endDate).format('YYYY-MM-DD'))
+    const prices = await fetchPriceHistory(iso, _startDate, _endDate)
+    setPriceHistory(prices)
+    console.log(prices)
+  }
+
+  const updateIsoValue = (e) => {
+    setIso(e.target.value)
+  }
+
+  const updateStartDate = (e) => {
+    setStartDate(e.target.value)
+  }
+
+  const updateEndDate = (e) => {
+    setEndDate(e.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <label htmlFor="iso">ISO</label>
+      <input type="text" id="iso" onChange={updateIsoValue} value={iso} />
+
+      <label htmlFor="startDate">Start date:</label>
+      <input type="date" id="startDate" name="startDate" value={startDate} onChange={updateStartDate} />
+
+      <label htmlFor="endDate">End date:</label>
+      <input type="date" id="endDate" name="endDate" value={endDate} onChange={updateEndDate} />
+
+      <button onClick={getPriceHistory}>Get history</button>
     </div>
   );
 }
--- src/utils/fetchPriceHistory.js
+++ src/utils/fetchPriceHistory.js
@@ -1,9 +1,9 @@
 
-const fetchPirceHistory =  async (iso, startDate, endDate) => {
+const fetchPriceHistory =  async (iso, startDate, endDate) => {
   try {
     const response = await fetch(`https://www.coindesk.com/pf/api/v3/content/fetch/chart-api?query={"end_date":"${endDate}","iso":"${iso}","ohlc":false,"start_date":"${startDate}"}&d=96&_website=coindesk`)
 
-    const data = response.json()
+    const data = await response.json()
     return data
   } catch (e) {
     console.log('---- ERROR FETCHING PRICE HISTORY ----', e)
@@ -11,4 +11,4 @@ const fetchPirceHistory =  async (iso, startDate, endDate) => {
   }
 }
 
-export default fetchPirceHistory
+export default fetchPriceHistory
