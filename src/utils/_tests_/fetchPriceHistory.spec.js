05ab8336ac9626e2d96cd5a0cfa4d36de880fe2d
Andrew Han <andrew@techsamurais.com>
2021-10-18 19:53:36 -0700

    write simple fetchPriceHistory function with test

---
+++ src/utils/_tests_/fetchPriceHistory.spec.js
@@ -0,0 +1,8 @@
+import fetchPriceHistory from '../fetchPriceHistory'
+
+describe('Fetch Price History using CoinDesk', () => {
+  it ('returns data', async () => {
+    const response = await fetchPriceHistory('ETH', '2021-10-12T01:34', '2021-10-19T01:34')
+    console.log(response)
+  })
+})
---
+++ src/utils/fetchPriceHistory.js
@@ -0,0 +1,14 @@
+
+const fetchPirceHistory =  async (iso, startDate, endDate) => {
+  try {
+    const response = await fetch(`https://www.coindesk.com/pf/api/v3/content/fetch/chart-api?query={"end_date":"${endDate}","iso":"${iso}","ohlc":false,"start_date":"${startDate}"}&d=96&_website=coindesk`)
+
+    const data = response.json()
+    return data
+  } catch (e) {
+    console.log('---- ERROR FETCHING PRICE HISTORY ----', e)
+    return e
+  }
+}
+
+export default fetchPirceHistory
