04469d67ae253a0ad69cc41b16a26e5a1f83d55d
Andrew Han <andrew@techsamurais.com>
2021-10-18 22:21:08 -0700

    create dropdown that saves coinId

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react'
 
-import Table from './components/Table'
-
-import './App.css';
 import fetchCoinList from './utils/fetchCoinList'
 
 const App = () =>  {
   const [coinList, setCoinList] = React.useState()
+  const [searchValue, setSearchValue] = React.useState('')
+  const [filteredCoinList, setFilteredCoinList] = React.useState()
+  const [displayDropdown, setDisplayDropdown] = React.useState()
+  const [selectedCoinId, setSelectedCoinId] = React.useState()
 
   const getCoinList = async () => {
     const _coinList = await fetchCoinList()
     setCoinList(_coinList)
-    console.log(_coinList)
   }
 
   React.useEffect(() => {
@@ -19,10 +19,68 @@ const App = () =>  {
       getCoinList()
     }
   }, [coinList])
-  
+
+  React.useMemo(() => {
+    // Debounce this
+    // Filter by symbol
+    if (coinList && searchValue) {
+      const _filteredCoinList = coinList.filter((coin) => {
+        return coin.name.toLowerCase().includes(searchValue.toLowerCase())
+      })
+      setFilteredCoinList(_filteredCoinList)
+    }
+    if (!searchValue) setDisplayDropdown(false)
+  }, [searchValue, coinList])
+
+  const updateSearchValue = (value) => {
+    setSearchValue(value)
+  }
+
+  const handleSearchInputChange = (e) => {
+    updateSearchValue(e.target.value)
+    setDisplayDropdown(true)
+  }
+
+  const handleDropdownItemClick = (coin) => () => {
+    updateSearchValue(coin.name)
+    setSelectedCoinId(coin.id)
+    setDisplayDropdown(false)
+  }
+
   return (
     <div className="App">
-      Hello World
+      <div style={{ border: '1px solid black'}}>
+        <input
+          type="text"
+          id="search"
+          name="search"
+          value={searchValue}
+          style={{ width: '100%' }}
+          onChange={handleSearchInputChange}
+        />
+        {
+          displayDropdown && (
+            <div style={{ height: 150, backgroundColor: 'grey', overflow: 'scroll' }}>
+              {
+                (filteredCoinList && filteredCoinList.length) ? filteredCoinList.map((coin) => {
+                  return (
+                    <div
+                      id={coin.id}
+                      onClick={handleDropdownItemClick(coin)}
+                      style={{ textAlign: 'left', cursor: 'pointer' }}
+                    >
+                      {coin.symbol}: {coin.name}
+                    </div>
+                  )
+                }) : (
+                  <div>There are no results.</div>
+                )
+              }
+            </div>
+            )
+        }
+      </div>
+      <button>Get price history</button>
     </div>
   );
 }
